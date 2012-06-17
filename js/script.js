6a2955ee512296950722e7775ac3144ee780ac56
Gabriel Florit <gabriel.florit@velir.com>
2012-06-17 14:07:40 -0400

    added codemirror

--- js/script.js
+++ js/script.js
@@ -1,6 +1,45 @@
-/* Author:
+/* Author: Gabriel Florit */
 
-*/
+var conditionOf = (function () {
+
+	return {
+
+		// resetBar: function(markerCenter) {
+		// },
+
+		// bar: null
+	}
+
+}());
+
+$(function() {
+
+	// create codemirror instance
+	conditionOf.codeMirror = CodeMirror($('#poem').get(0), {
+
+		mode:  'javascript',
+
+		onCursorActivity: function(cm) {
+
+			var cursor = cm.getCursor();
+			var token = cm.getTokenAt(cursor);
+			$('#test').text(token.string);
+
+		}
+	});
+
+	// load all the reasons in memory
+	var reasons = [
+		'for fear of alienating a landlord.',
+		'because of fears that he too could be kidnapped.',
+		'because the subject is politically sensitive.',
+		'because she lives alone'
+	];
+
+	conditionOf.codeMirror.setValue('for fear of alienating a landlord.\nbecause of fears that he too could be kidnapped.\n');
+
+});
+
 
 
 
